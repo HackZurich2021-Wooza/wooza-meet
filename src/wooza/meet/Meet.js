36829559e4d154b88cc0df3653692759b7c31d1c
Sven Stucki <stucki@procivis.ch>
2021-09-25 14:16:24 +0200

    Update

---
+++ src/wooza/meet/Meet.js
@@ -0,0 +1,48 @@
+
+const JitsiMeetJS = window.JitsiMeetJS;
+
+function joinMeeting(meetingStr) {
+  console.log('init Jitsi');
+  JitsiMeetJS.init();
+
+  console.log('setup connection');
+  const conn = new JitsiMeetJS.JitsiConnection(
+    null,
+    null,
+    {
+      hosts: {
+        domain: 'alpha.jitsi.net',
+        muc: 'conference.alpha.jitsi.net',
+      },
+    },
+  );
+
+  conn.addEventListener(
+    JitsiMeetJS.events.connection.CONNECTION_ESTABLISHED,
+    (evt) => console.log('connection succes with args', arguments),
+  );
+  conn.addEventListener(
+    JitsiMeetJS.events.connection.CONNECTION_FAILED,
+    (evt) => console.log('connection failed with args', arguments),
+  );
+  conn.addEventListener(
+    JitsiMeetJS.events.connection.CONNECTION_DISCONNECTED,
+    (evt) => console.log('connection disconnect with args', arguments),
+  );
+
+  console.log('start connection');
+  conn.connect();
+
+
+}
+
+
+
+export default function Meet() {
+  return (
+    <>
+      <h1>Meet</h1>
+      <button onClick={joinMeeting.bind('ClassicDecembersRetainEventually')}>JOINNNNNN</button>
+    </>
+  );
+}
