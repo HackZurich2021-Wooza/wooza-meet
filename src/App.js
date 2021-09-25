994c7ee56b9d46adc3ebe28dddb99091f2f72d37
Alexandre Moeri <alex.moeri@gmail.com>
2021-09-25 16:01:26 +0200

    setup proton

--- src/App.js
+++ src/App.js
@@ -1,14 +1,49 @@
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 import './App.css';
 import Meet from './wooza/meet/Meet';
+import Game from './wooza/game/Game';
+import Setup from './wooza/setup/Setup';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Meet />
-      </header>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Meet</Link>
+              </li>
+              <li>
+                <Link to="/game">Game</Link>
+              </li>
+              <li>
+                <Link to="/setup">Setup</Link>
+              </li>
+            </ul>
+          </nav>
+
+          {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/game">
+              <Game />
+            </Route>
+            <Route path="/setup">
+              <Setup />
+            </Route>
+            <Route path="/">
+              <Meet />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
--- src/wooza/game/Game.js
+++ src/wooza/game/Game.js
@@ -1,6 +1,49 @@
+import Proton from "proton-engine";
+import RAFManager from 'raf-manager';
+
+import { useEffect, useRef } from "react";
+
 
 export default function Game() {
+
+  const canvas = useRef(null);
+
+
+  useEffect(() => {
+
+    const proton = new Proton();
+    const emitter = new Proton.Emitter();
+
+    //set Rate
+    emitter.rate = new Proton.Rate(Proton.getSpan(10, 20), 0.1);
+
+    //add Initialize
+    emitter.addInitialize(new Proton.Radius(1, 12));
+    emitter.addInitialize(new Proton.Life(2, 4));
+    emitter.addInitialize(new Proton.Velocity(3, Proton.getSpan(0, 360), "polar"));
+
+    //add Behaviour
+    emitter.addBehaviour(new Proton.Color("ff0000", "random"));
+    emitter.addBehaviour(new Proton.Alpha(1, 0));
+
+    //set emitter position
+    emitter.p.x = canvas.current.width / 2;
+    emitter.p.y = canvas.current.height / 2;
+    emitter.emit(5);
+
+    //add emitter to the proton
+    proton.addEmitter(emitter);
+
+    // add canvas renderer
+    const renderer = new Proton.CanvasRenderer(canvas.current);
+    proton.addRenderer(renderer);
+
+    RAFManager.add(() => { proton.update(); })
+  }, [canvas]
+  );
   return (
-    <h1>Game</h1>
+    <>
+      <canvas ref={canvas} width="500px" height="500px"></canvas>
+    </>
   );
 }
